79a0ccb5dbe7696c25d9bf7336bea3875616ecf1
msv37 <131259473+msv37@users.noreply.github.com>
2023-06-12 22:37:57 +0200

    Wrote a subheading

--- test.py
+++ test.py
@@ -13,6 +13,8 @@ st.dataframe(df)
 df_max = df.groupby(['Job Title','Years of Experience'])['Salary'].max()
 st.dataframe(df_max)
 
+st.write("Salaries in order from greatest to lowest")
+
 df_max2 = df.groupby(['Job Title'])['Salary'].max()
 df_max2 = df.sort_values(by=['Salary'], ascending=False)
 st.dataframe(df_max2)
