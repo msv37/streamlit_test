72d7adfe3b2e5013ba7d63d95039d3aef71d7361
msv37 <131259473+msv37@users.noreply.github.com>
2023-06-12 20:35:21 +0200

    Dataframe Test

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
 import streamlit as st
+import pandas as pd 
+import numpy as np
+
+
 st.write("Hello World")
+
+df= pd.DataFrame({"Name": "Maria", "Age": 15, "School": "Rosenberg"})
+st.dataframe(df)
